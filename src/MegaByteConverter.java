4ce10bc32bb5e9cc7d1ec4fc3f7b23619645a66f
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-07-30 16:34:17 +0100

    kiloBytes

--- src/MegaByteConverter.java
+++ src/MegaByteConverter.java
@@ -3,7 +3,9 @@ public class MegaByteConverter {
         if(kiloBytes < 0) {
             System.out.println("Invalid Value");
         }else{
-            System.out.println();
+            int MB = kiloBytes/1024;
+            int KB = kiloBytes%1024;
+            System.out.println(kiloBytes + "KB" = MB + "MB" + "and" + KB + "KB");
             }
     }
 }
